ab5c52e76e28a1db20d8be51b13e59544156dbf7
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2023-12-27 02:08:57 +0530

    MacBook page is created

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,20 +1,30 @@
-// src/components/HomePage.js
 import React from 'react';
+import { useState } from 'react';
+import database from '../firebase';
 import { Link } from 'react-router-dom';
-import centerImage from '../image/dili.png'; // Import your image
-import '../components/HomePage.css'; // Import your custom CSS file
-
+import centerImage from '../image/2nd.png';
+import '../components/HomePage.css';
 
 const HomePage = () => {
-  const pageStyle = {
-    background: 'white', // Set the background color of the page to red
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+
+  const Push = () => {
+    database.ref('user').set({
+      name: name,
+      age: age,
+    }).catch(alert);
   };
 
- 
-  const spacedTextStyle = {
-    letterSpacing: '25px', // Set the spacing between letters
+  const columnStyle = {
+    flex: 1,
+    padding: '20px',
+    boxSizing: 'border-box', // Include padding in the column size
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   };
-
   const imageContainerStyle = {
     margin: 'center', // Adjust the margin around the image
     display: 'flex',
@@ -24,21 +34,48 @@ const HomePage = () => {
     minHeight: '100vh',
   };
 
+  const pageStyle = {
+    background: 'white',
+  };
+
   return (
     <div style={pageStyle}>
       <header className="header">
         {/* You can add navigation links here if needed */}
       </header>
 
-      <div style={imageContainerStyle}>
-        <img src={centerImage} alt="Centered Image" />
+      <div className="App" style={{ marginTop: 50, display: 'flex' }}>
+        {/* First Column - Image */}
+        <div style={imageContainerStyle}>
+          <img src={centerImage} alt="Centered Image" />
+        </div>
+
+        {/* Second Column - Form */}
+        <div style={columnStyle}>
+          <center>
+            <input
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <br />
+            <br />
+            <input
+              placeholder="Enter your age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+            <br />
+            <br />
+            <button onClick={Push}>PUSH</button>
+          </center>
+        </div>
       </div>
 
       <footer className="footer">
-        <p style={{ ...spacedTextStyle, fontSize: '30px', textAlign: 'center' }}>GENERATOR X INDUSTRIES</p>
+        <p style={{ fontSize: '30px', textAlign: 'center' }}>GENERATOR X INDUSTRIES</p>
         <p style={{ fontSize: '15px', textAlign: 'center' }}>Copyright © 2023 All rights reserved by AD Printers</p>
-        {/* Link to another page */}
-        <Link to="./contact">Go to Another Page</Link>
+        <Link to="./contact">Go to Another Page</Link> <Link to="./macbook">Go to Another Page</Link>
       </footer>
     </div>
   );
--- src/pages/MacBook.js
+++ src/pages/MacBook.js
@@ -1,18 +1,48 @@
-// src/components/HomePage.js
-import React from 'react';
-import '../components/HomePage.css'; // Import your custom CSS file
+import { useState } from 'react';
+import database from '../firebase';
 
+function App() {
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
 
-const HomePage = () => {
-
+  // Push Function
+  const Push = () => {
+    database.ref('user').set({
+      name: name,
+      age: age,
+    }).catch(alert);
+  };
 
   return (
-    <div >
-   uyhiughiiuk
+    <div className="App" style={{ marginTop: 50, display: 'flex' }}>
+      {/* First Column - Form */}
+      <div style={{ flex: 1, padding: '20px' }}>
+        <center>
+          <input
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            placeholder="Enter your age"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+          <br />
+          <br />
+          <button onClick={Push}>PUSH</button>
+        </center>
+      </div>
 
-     
+      {/* Second Column - Description */}
+      <div style={{ flex: 1, padding: '20px' }}>
+        <h2>Description</h2>
+        <p>Add your description content here.</p>
+      </div>
     </div>
   );
-};
+}
 
-export default HomePage;
+export default App;
