343027462c89b0d887c88e27a5c17c106b79f52b
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-03-07 23:43:55 +0530

    Home page css ,  Loading page css

--- src/pages/8OperatorPage3/8OperatorPage3.js
+++ src/pages/8OperatorPage3/8OperatorPage3.js
@@ -13,24 +13,18 @@ const OperatorPage3 = () => {
 
   return (
     <div className="pageStyle"> 
-      <header className="header"> </header>
-      <div id='A_P_3_images' className="A_P_3_images">
-        <img src={gen_X} alt="gen_x Image" className="gen_X" />
-        <div id='A_P_3_logout' className="A_P_3_logout">
-          <img src={icon} alt="Icon" className="A_P_3_icon1" />
-          <button onClick={handleLogout} className="A_P_3_icon2">
-            Log Off
-          </button>
-        </div>
-      </div>
-      <div className="imageContainerStyle">
-        Generator List
-      </div>
+    <header>
+        <h1>Your Website Header</h1>
+    </header>
 
-      <footer className="footer">
-        <p className="spacedTextStyle footerTextStyle1">GENERATOR X INDUSTRIES</p>
-        <p className="footerTextStyle2">Copyright © 2023 All rights reserved by AD Printers</p>
-      </footer>
+    <main>
+        <p>This is the main content of your web page.</p>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Your Website. All rights reserved.</p>
+    </footer>
+
     </div>
   );
 };
